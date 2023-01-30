a47cba6de178dac03c707fb2c44533f1c3543c77
alpaternina <alpaternina@hotmail.com>
2023-01-29 22:50:17 -0500

    Preentrega 1 React

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+import { Navbar } from './Navbar/Navbar';
+
+export const App = () => {
+
+  return (
+
+    <>
+      <Navbar nombre= {"Bombas Centrifugas"}/>
+    </>
+   
+  )
+
+}
